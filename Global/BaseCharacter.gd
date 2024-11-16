b18a54687d26c29004cc8b90f01b21dbef871e2f
Eugene Kovs <smartkovsik@gmail.com>
2024-11-16 15:39:13 +0900

    Global objects

---
+++ Global/BaseCharacter.gd
@@ -0,0 +1,11 @@
+# BaseCharacter.gd
+# Used CharacterBody2D global methods across the project
+extends CharacterBody2D
+
+var speed: float = 100.0  # Default speed, can be overridden
+
+func move_character(delta: float) -> void:
+	move_and_collide(Vector2.DOWN * speed * delta)
+
+func handle_screen_exit() -> void:
+	queue_free()
